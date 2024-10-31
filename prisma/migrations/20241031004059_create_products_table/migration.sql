9a93a8fe97b226b651dacfe54cb9eca44816b718
Jose Velasco <josepe_789@hotmail.com>
2024-10-30 22:12:31 -0400

    Add Prisma Service for database connection

---
+++ prisma/migrations/20241031004059_create_products_table/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Products` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Products";
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "priceSale" DOUBLE PRECISION NOT NULL,
+    "pricePurchase" DOUBLE PRECISION NOT NULL,
+    "slug" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "isAvailable" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_name_key" ON "products"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_slug_key" ON "products"("slug");
